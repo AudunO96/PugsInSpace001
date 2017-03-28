3d9988f7d75cc8f9549991341f979501f6d34641
AudunO96 <audiso96@gmail.com>
2017-03-28 16:40:33 +0200

    Fixed some stuff

---
+++ Source/PugsInSpace001/LevelDoor.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "LevelDoor.generated.h"
+
+UCLASS()
+class PUGSINSPACE001_API ALevelDoor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ALevelDoor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION()
+		void OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor *OtherActor,
+			UPrimitiveComponent *OtherComponent, int32 OtherBodyIndex,
+			bool bFromSweep, const FHitResult &SweepResult);
+
+	UFUNCTION()
+		void OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+	
+	UFUNCTION()
+		void OpenDoor();
+
+	UPROPERTY(EditAnywhere)
+		FName LoadMapName;
+
+	UShapeComponent* CollisionBox = nullptr;
+
+
+
+};
