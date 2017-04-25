8d4e1ef4b5057af163c24baa2b5a0e9fe6285083
TheWeirdNorwegianGuy <the_weird_norwegian_guy@hotmail.co.uk>
2017-04-25 16:03:41 +0200

    Elevator
    Finishing the code for the elevator in Storage.

--- Source/PugsInSpace001/ElevatorFloor.cpp
+++ Source/PugsInSpace001/ElevatorFloor.cpp
@@ -24,18 +24,51 @@ void AElevatorFloor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	if (toggleSwitch == false)
-	{
-		if (goDown == true)
-		{
+	FVector position = GetActorLocation();
 
+	if (toggleSwitch == true)
+	{
+		if (isMoving == false)
+		{
+			if (position.Z == maxZ)
+			{
+				goDown = true;
+				isMoving = true;
+
+			}
+			else if (position.Z == minZ)
+			{
+				goDown = false;
+				isMoving = true;
+			}
+		}
+	}
+
+	if (isMoving == true)
+	{
+		if (position.Z >= maxZ || position.Z <= minZ)
+		{
+			isMoving = false;
 		}
 
-		else if (goUp == true)
+		else
 		{
-
+			if (goDown == true)
+			{
+				position.Z = position.Z - speed;
+				SetActorLocation(position);
+			}
+			else if (goDown == false)
+			{
+				position.Z = position.Z + speed;
+				SetActorLocation(position);
+			}
 		}
 	}
 
 }
 
+void AElevatorFloor::MoveElevator(bool getToggle)
+{
+	toggleSwitch = getToggle;
+}
