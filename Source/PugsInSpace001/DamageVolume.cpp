4e4aaad5a74163fcd216d25f5d68a8ce284cd978
AudunO96 <audiso96@gmail.com>
2017-04-27 21:57:01 +0200

    Added deathboxes

---
+++ Source/PugsInSpace001/DamageVolume.cpp
@@ -0,0 +1,52 @@
+// Coded by Audun Olsen and Henrik Engenes 2016/17
+
+#include "PugsInSpace001.h"
+#include "DamageVolume.h"
+#include "PugCharacter.h"
+
+
+// Sets default values
+ADamageVolume::ADamageVolume()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ADamageVolume::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ADamageVolume::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ADamageVolume::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor *OtherActor,
+	UPrimitiveComponent *OtherComponent, int32 OtherBodyIndex,
+	bool bFromSweep, const FHitResult &SweepResult)
+{
+	if (OtherActor->IsA(APugCharacter::StaticClass()))
+	{
+		APugCharacter* Puglet = Cast<APugCharacter>(OtherActor);
+		Puglet->DeathObj = this;
+		Puglet->OnDeath();
+
+	}
+}
+
+
+void ADamageVolume::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	if (OtherActor->IsA(APugCharacter::StaticClass()))
+	{
+		APugCharacter* Puglet = Cast<APugCharacter>(OtherActor);
+		Puglet->DeathObj = nullptr;
+
+	}
+}
