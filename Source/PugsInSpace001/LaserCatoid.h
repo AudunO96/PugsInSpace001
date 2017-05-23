d45ff3157968478de72a77b5fcb3de7df731f5d5
TheWeirdNorwegianGuy <the_weird_norwegian_guy@hotmail.co.uk>
2017-05-23 21:53:18 +0200

    HHHHHHH

--- Source/PugsInSpace001/LaserCatoid.h
+++ Source/PugsInSpace001/LaserCatoid.h
@@ -16,6 +16,9 @@ public:
 	// Sets default values for this character's properties
 	ALaserCatoid();
 
+	UPROPERTY(EditAnywhere, Category = "Behaviour")
+		class UBehaviourTree *BotBehaviour;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
