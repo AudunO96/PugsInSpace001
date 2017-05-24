8aeb0727253e688bfedc3658049824cb6baf175d
TheWeirdNorwegianGuy <the_weird_norwegian_guy@hotmail.co.uk>
2017-05-24 06:34:39 +0200

    LASERS
    Maybe they'll finally work

--- Source/PugsInSpace001/LaserCatAI.h
+++ Source/PugsInSpace001/LaserCatAI.h
@@ -12,8 +12,22 @@ UCLASS()
 class PUGSINSPACE001_API ALaserCatAI : public AAIController
 {
 	GENERATED_BODY()
-	
-	
+
+	UPROPERTY(transient)
+	class UBlackboardComponent *BlackboardComp;
+			
+	UPROPERTY(transient)
+	class UBehaviorTreeComponent *BehaviorComp;
+
+public:
+
+	ALaserCatAI();
+
+	class ALaserCatoid *LaserChar;
+
+	virtual void Possess(APawn *InPawn) override;
+
+	uint8 EnemyKeyID;
 	
 	
 };
